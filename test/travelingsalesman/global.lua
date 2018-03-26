0acd2a4b92313693af87e2e28cb90434f31276da
totalwarANGEL <totalwarANGEL1993@web.de>
2018-03-26 19:44:04 +0200

    Bugfix: Traveling Salesman

---
+++ test/travelingsalesman/global.lua
@@ -0,0 +1,74 @@
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Mission_InitPlayers
+----------------------------------
+-- Diese Funktion kann benutzt werden um für die AI
+-- Vereinbarungen zu treffen.
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function Mission_InitPlayers()
+end
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Mission_SetStartingMonth
+----------------------------------
+-- Diese Funktion setzt einzig den Startmonat.
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function Mission_SetStartingMonth()
+    Logic.SetMonthOffset(3);
+end
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Mission_InitMerchants
+----------------------------------
+-- Hier kannst du Hдndler und Handelsposten vereinbaren.
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function Mission_InitMerchants()
+end
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Mission_FirstMapAction
+----------------------------------
+-- Die FirstMapAction wird am Spielstart aufgerufen.
+-- Starte von hier aus deine Funktionen.
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function Mission_FirstMapAction()
+    Script.Load("E:/Repositories/symfonia/test/travelingsalesman/qsb.lua")
+    API.Install()
+
+    if Framework.IsNetworkGame() ~= true then
+        Startup_Player()
+        Startup_StartGoods()
+        Startup_Diplomacy()
+    end
+    
+    API.ActivateDebugMode(true, true, true, true)
+    
+    AddGood(Goods.G_Gold,   500, 1)
+    AddGood(Goods.G_Wood,    30, 1)
+    AddGood(Goods.G_Grain,   25, 1)
+    
+    -----
+    
+    local offers = {
+    {
+    {"G_Gems",5,},
+    {"G_Iron",5,},
+    {"G_Beer",2,},
+    },
+    {
+    {"G_Stone",5,},
+    {"G_Sheep",1,},
+    {"G_Cheese",2,},
+    },
+    {
+    {"G_Grain",5,},
+    {"G_Broom",2,},
+    {"G_Sheep",1,},
+    },
+    {
+    {"U_CatapultCart",1,},
+    {"U_MilitarySword",3,},
+    {"U_MilitaryBow",3,},
+    },
+    }
+    API.ActivateTravelingSalesman(2, offers, nil, {"WP1", "WP2", "WP3", "WP4"}, nil);
+end
