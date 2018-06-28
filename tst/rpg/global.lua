4c7c7dd0274c3c37c769b133868102916f75d63f
totalwarANGEL <totalwarANGEL1993@web.de>
2018-06-28 19:57:02 +0200

    Inventaranzeige getestet

--- tst/rpg/global.lua
+++ tst/rpg/global.lua
@@ -41,5 +41,50 @@ function Mission_FirstMapAction()
         Startup_Diplomacy()
     end
     
-    API.ActivateDebugMode(true, true, true, true)
+    API.ActivateDebugMode(true, true, true, true);
+    
+    
+    
+end
+
+--
+-- Test Case: Rucksack/Ausrüstung anzeigen
+--
+-- Erstellt einige Testgegenstände und einen Helden um die Anzeige der
+-- Gegenstände zu testen. Es soll zwischen Rucksack und Ausrüstung
+-- umhergesprungen werden können.
+--
+-- Wenn ein Gegenstand ausgerüstet wird, wird die Menge der Gegenstände
+-- dieses Typs um 1 reduziert. Ist schon ein Gegenstand ausgerüstet, wird
+-- der wieder ins Inventar zurückgelegt.
+--
+function TestInventoryListing()
+    local Dummy1 = AddOnRolePlayingGame.Item:New("Dummy1");
+    Dummy1:SetCaption("Dummy 1");
+    Dummy1:SetDescription("This ist Dummy 1!");
+    
+    local Dummy2 = AddOnRolePlayingGame.Item:New("Dummy2");
+    Dummy2:SetCaption("Dummy 2");
+    Dummy2:SetDescription("This ist Dummy 2!");
+    
+    local Dummy3 = AddOnRolePlayingGame.Item:New("Dummy3");
+    Dummy3:SetCaption("Dummy 3");
+    Dummy3:SetDescription("This ist Dummy 3!");
+    
+    local Dummy4 = AddOnRolePlayingGame.Item:New("Dummy4");
+    Dummy4:SetCaption("Dummy 4");
+    Dummy4:SetDescription("This ist Dummy 4!");
+    
+    
+    local Meredith = AddOnRolePlayingGame.Hero:New("meredith");
+    local Inventory = AddOnRolePlayingGame.Inventory:New("Inventory_Meredith", Meredith);
+    Inventory.Owner = Meredith;
+    Meredith.Inventory = Inventory;
+    
+    Inventory:Insert("Dummy1", 10);
+    Inventory:Insert("Dummy2", 3);
+    Inventory:Insert("Dummy3", 7);
+    Inventory:Insert("Dummy4", 1);
+    
+    Inventory:Equip("Dummy4");
 end
