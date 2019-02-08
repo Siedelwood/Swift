8ad726327cee9087fde735c2b36addf546f9d2bf
totalwarANGEL <totalwarANGEL1993@web.de>
2019-02-08 17:02:27 +0100

    shortening time line

--- qsb/lua/bundles/bundletimeline.lua
+++ qsb/lua/bundles/bundletimeline.lua
@@ -28,9 +28,12 @@ BundleTimeLine = {};
 API = API or {};
 QSB = QSB or {};
 
+QSB.TimeLine = {};
+
 -- -------------------------------------------------------------------------- --
--- User-Space                                                                 --
+-- TimeLine Klasse                                                            --
 -- -------------------------------------------------------------------------- --
+
 ---
 -- Startet einen Zeitstrahl. Ein Zeitstrahl hat Stationen,
 -- an denen eine Aktion ausgeführt wird. Jede Station muss mindestens eine
@@ -38,8 +41,6 @@ QSB = QSB or {};
 --
 -- Jede Aktion eines Zeitstrahls erhält die Table des aktuellen Ereignisses
 -- als Argument. So können Parameter an die Funktion übergeben werden.
---
--- <p><b>Alias:</b> QSB.TimeLine:Start<br></p>
 --
 -- @param[type=table] _Description Beschreibung
 -- @return[type=number] ID des Zeitstrahls
@@ -53,29 +54,51 @@ QSB = QSB or {};
 --     {Time = 49, Action = function() end},
 -- }
 --
-function API.TimeLineStart(_Description)
-    return BundleTimeLine.Shared.TimeLine:Start(_Description);
+function QSB.TimeLine:Start(_description)
+    local JobID = self.Data.TimeLineUniqueJobID;
+    self.Data.TimeLineUniqueJobID = JobID +1;
+
+    _description.Running = true;
+    _description.StartTime = Logic.GetTime();
+    _description.Iterator = 1;
+
+    -- Check auf sinnvolle Zeitabstände
+    local Last = 0;
+    for i=1, #_description, 1 do
+        if _description[i].Time < Last then
+            _description[i].Time = Last+1;
+            Last = _description[i].Time;
+        end
+    end
+
+    self.Data.TimeLineJobs[JobID] = _description;
+    if not self.Data.ControlerID then
+        local Controler = StartSimpleHiResJobEx(QSB.TimeLine.TimeLineControler);
+        self.Data.ControlerID = Controler;
+    end
+    return JobID;
 end
 
 ---
 -- Startet einen Zeitstrahl erneut. Ist der Zeitstrahl noch nicht
 -- beendet, beginnt er dennoch von vorn.
 --
--- <p><b>Alias:</b> QSB.TimeLine:Restart<br></p>
---
 -- @param[type=number] _ID ID des Zeitstrahl
 -- @within Anwenderfunktionen
 --
 -- @usage API.TimeLineRestart(MyTimeLine);
 --
-function API.TimeLineRestart(_ID)
-    return BundleTimeLine.Shared.TimeLine:Restart(_ID)
+function QSB.TimeLine:Restart(_ID)
+    if not self.Data.TimeLineJobs[_ID] then
+        return;
+    end
+    self.Data.TimeLineJobs[_ID].Running = true;
+    self.Data.TimeLineJobs[_ID].StartTime = Logic.GetTime();
+    self.Data.TimeLineJobs[_ID].Iterator = 1;
 end
 
 ---
 -- Prüft, ob der Zeitstrahl noch nicht durchgelaufen ist.
---
--- <p><b>Alias:</b> QSB.TimeLine:IsRunning<br></p>
 --
 -- @param[type=number] _ID ID des Zeitstrahl
 -- @return[type=boolean] Zeitstrahl ist aktiv
@@ -83,36 +106,68 @@ end
 --
 -- @usage local IsRunning = API.TimeLineIsRunning(MyTimeLine);
 --
-function API.TimeLineIsRunning(_ID)
-    return BundleTimeLine.Shared.TimeLine:IsRunning(_ID);
+function QSB.TimeLine:IsRunning(_ID)
+    if self.Data.TimeLineJobs[_ID] then
+        return self.Data.TimeLineJobs[_ID].Running == true;
+    end
+    return false;
 end
 
 ---
 -- Hält einen Zeitstrahl an.
---
--- <p><b>Alias:</b> QSB.TimeLine:Yield<br></p>
 --
 -- @param[type=number] _ID ID des Zeitstrahl
 -- @within Anwenderfunktionen
 --
 -- @usage API.TimeLineYield(MyTimeLine);
 --
-function API.TimeLineYield(_ID)
-    return BundleTimeLine.Shared.TimeLine:Yield(_ID);
+function QSB.TimeLine:Yield(_ID)
+    if not self.Data.TimeLineJobs[_ID] then
+        return;
+    end
+    self.Data.TimeLineJobs[_ID].YieldTime = Logic.GetTime();
+    self.Data.TimeLineJobs[_ID].Running = false;
 end
 
 ---
 -- Stößt einen angehaltenen Zeitstrahl wieder an.
---
--- <p><b>Alias:</b> QSB.TimeLine:Resume<br></p>
 --
 -- @param[type=number] _ID ID des Zeitstrahl
 -- @within Anwenderfunktionen
 --
 -- @usage API.TimeLineResume(MyTimeLine);
 --
-function API.TimeLineResume(_ID)
-    return BundleTimeLine.Shared.TimeLine:Resume(_ID);
+function QSB.TimeLine:Resume(_ID)
+    if not self.Data.TimeLineJobs[_ID] then
+        return;
+    end
+    if self.Data.TimeLineJobs[_ID].YieldTime then
+        local OldStartTime = self.Data.TimeLineJobs[_ID].StartTime;
+        local TimeYielded = Logic.GetTime() - self.Data.TimeLineJobs[_ID].YieldTime;
+        self.Data.TimeLineJobs[_ID].StartTime = OldStartTime + TimeYielded;
+        self.Data.TimeLineJobs[_ID].YieldTime = nil;
+    end
+    self.Data.TimeLineJobs[_ID].Running = true;
+end
+
+---
+-- Steuert alle Zeitstrahlen.
+-- @within QSB.TimeLine
+-- @local
+--
+function QSB.TimeLine.TimeLineControler()
+    for k,v in pairs(QSB.TimeLine.Data.TimeLineJobs) do
+        if v.Iterator > #v then
+            QSB.TimeLine.Data.TimeLineJobs[k].Running = false;
+        end
+
+        if v.Running then
+            if (v[v.Iterator].Time + v.StartTime) <= Logic.GetTime() then
+                v[v.Iterator].Action(unpack(v[v.Iterator]));
+                QSB.TimeLine.Data.TimeLineJobs[k].Iterator = v.Iterator +1;
+            end
+        end
+    end
 end
 
 -- -------------------------------------------------------------------------- --
@@ -145,7 +200,6 @@ BundleTimeLine = {
 -- @local
 --
 function BundleTimeLine.Global:Install()
-    QSB.TimeLine = BundleTimeLine.Shared.TimeLine;
     TimeLine = QSB.TimeLine;
 end
 
@@ -158,146 +212,7 @@ end
 -- @local
 --
 function BundleTimeLine.Local:Install()
-    QSB.TimeLine = BundleTimeLine.Shared.TimeLine;
     TimeLine = QSB.TimeLine;
-end
-
--- Shared Script ------------------------------------------------------------ --
-
----
--- Startet einen Zeitstrahl. Ein Zeitstrahl hat bestimmte Stationen,
--- an denen eine Aktion ausgeführt wird.
---
--- <p><b>Alias:</b> QSB.TimeLine:Start<br></p>
---
--- @param[type=table] _description Beschreibung
--- @param[type=number] _ID ID des Zeitstrahl
--- @within QSB.TimeLine
--- @local
---
-function BundleTimeLine.Shared.TimeLine:Start(_description)
-    local JobID = self.Data.TimeLineUniqueJobID;
-    self.Data.TimeLineUniqueJobID = JobID +1;
-
-    _description.Running = true;
-    _description.StartTime = Logic.GetTime();
-    _description.Iterator = 1;
-
-    -- Check auf sinnvolle Zeitabstände
-    local Last = 0;
-    for i=1, #_description, 1 do
-        if _description[i].Time < Last then
-            _description[i].Time = Last+1;
-            Last = _description[i].Time;
-        end
-    end
-
-    self.Data.TimeLineJobs[JobID] = _description;
-    if not self.Data.ControlerID then
-        local Controler = StartSimpleHiResJobEx(BundleTimeLine.Shared.TimeLine.TimeLineControler);
-        self.Data.ControlerID = Controler;
-    end
-    return JobID;
-end
-
----
--- Startet einen Zeitstrahl erneut. Ist der Zeitstrahl noch nicht
--- beendet, beginnt er dennoch von vorn.
---
--- <p><b>Alias:</b> QSB.TimeLine:Restart<br></p>
---
--- @param[type=number] _ID ID des Zeitstrahl
--- @within QSB.TimeLine
--- @local
---
-function BundleTimeLine.Shared.TimeLine:Restart(_ID)
-    if not self.Data.TimeLineJobs[_ID] then
-        return;
-    end
-    self.Data.TimeLineJobs[_ID].Running = true;
-    self.Data.TimeLineJobs[_ID].StartTime = Logic.GetTime();
-    self.Data.TimeLineJobs[_ID].Iterator = 1;
-end
-
----
--- Prüft, ob der Zeitstrahl noch nicht durchgelaufen ist.
---
--- <p><b>Alias:</b> QSB.TimeLine:IsRunning<br></p>
---
--- @param[type=number] _ID ID des Zeitstrahl
--- @return[type=boolean] Zeistrahl ist aktiv
--- @within QSB.TimeLine
--- @local
---
-function BundleTimeLine.Shared.TimeLine:IsRunning(_ID)
-    if self.Data.TimeLineJobs[_ID] then
-        return self.Data.TimeLineJobs[_ID].Running == true;
-    end
-    return false;
-end
-
----
--- Hält einen Zeitstrahl an.
---
--- Der Zeitstempel wird gespeichert.
---
--- <p><b>Alias:</b> QSB.TimeLine:Yield<br></p>
---
--- @param[type=number] _ID ID des Zeitstrahl
--- @within QSB.TimeLine
--- @local
---
-function BundleTimeLine.Shared.TimeLine:Yield(_ID)
-    if not self.Data.TimeLineJobs[_ID] then
-        return;
-    end
-    self.Data.TimeLineJobs[_ID].YieldTime = Logic.GetTime();
-    self.Data.TimeLineJobs[_ID].Running = false;
-end
-
----
--- Stößt einen angehaltenen Zeitstrahl wieder an.
---
--- Die Zeit, die seit der Pausierung vergangen ist, wird zur Startzeit
--- addiert um Trigger Fehler zu vermeiden.
---
--- <p><b>Alias:</b> QSB.TimeLine:Resume<br></p>
---
--- @param[type=number] _ID ID des Zeitstrahl
--- @within QSB.TimeLine
--- @local
---
-function BundleTimeLine.Shared.TimeLine:Resume(_ID)
-    if not self.Data.TimeLineJobs[_ID] then
-        return;
-    end
-    if self.Data.TimeLineJobs[_ID].YieldTime then
-        local OldStartTime = self.Data.TimeLineJobs[_ID].StartTime;
-        local TimeYielded = Logic.GetTime() - self.Data.TimeLineJobs[_ID].YieldTime;
-        self.Data.TimeLineJobs[_ID].StartTime = OldStartTime + TimeYielded;
-        self.Data.TimeLineJobs[_ID].YieldTime = nil;
-    end
-    self.Data.TimeLineJobs[_ID].Running = true;
-end
-
----
--- Steuert alle Zeitstrahlen.
--- @within QSB.TimeLine
--- @local
---
-function BundleTimeLine.Shared.TimeLine.TimeLineControler()
-    for k,v in pairs(BundleTimeLine.Shared.TimeLine.Data.TimeLineJobs) do
-        if v.Iterator > #v then
-            BundleTimeLine.Shared.TimeLine.Data.TimeLineJobs[k].Running = false;
-        end
-
-        if v.Running then
-            if (v[v.Iterator].Time + v.StartTime) <= Logic.GetTime() then
-                v[v.Iterator].Action(unpack(v[v.Iterator]));
-                BundleTimeLine.Shared.TimeLine.Data.TimeLineJobs[k].Iterator = v.Iterator +1;
-            end
-        end
-    end
 end
 
 -- -------------------------------------------------------------------------- --
