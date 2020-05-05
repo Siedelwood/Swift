648aeb5a67b757c047a265ff60d26ea42217e674
totalwarANGEL <totalwarANGEL1993@web.de>
2020-05-05 19:11:13 +0200

    adds harbor demo map

--- tst/travelingsalesman/global.lua
+++ tst/travelingsalesman/global.lua
@@ -31,8 +31,8 @@ end
 -- Starte von hier aus deine Funktionen.
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function Mission_FirstMapAction()
-    Script.Load("E:/Repositories/symfonia/var/qsb.lua")
-    API.Install()
+    Script.Load("E:/Repositories/symfonia/var/qsb.lua");
+    API.Install();
 
     if Framework.IsNetworkGame() ~= true then
         Startup_Player()
@@ -42,38 +42,131 @@ function Mission_FirstMapAction()
     
     API.ActivateDebugMode(true, false, true, true)
     
-    AddGood(Goods.G_Gold,   500, 1)
-    AddGood(Goods.G_Wood,    30, 1)
-    AddGood(Goods.G_Grain,   25, 1)
+    AddGood(Goods.G_Gold, 50000, 1);
     
     -----
     
+    SetDiplomacyState(1, 2, 2);
+    SetDiplomacyState(1, 3, 2);
+    SetDiplomacyState(1, 4, 2);
+    SetDiplomacyState(1, 5, 2);
+
+    API.InterfaceSetPlayerColor(2, 6);
+    API.InterfaceSetPlayerColor(3, 2);
+    API.InterfaceSetPlayerColor(4, 3);
+    API.InterfaceSetPlayerColor(5, 4);
+
+    API.InterfaceSetPlayerPortrait(2);
+    API.InterfaceSetPlayerPortrait(3);
+    API.InterfaceSetPlayerPortrait(4);
+    API.InterfaceSetPlayerPortrait(5);
+
+    CreateHabor2();
+    CreateHabor3();
+    CreateHabor4();
+    CreateHabor5();
+end
+
+function CreateHabor2()
     local TraderDescription = {
-        PlayerID = 2,
-        Waypoints = {"WP1", "WP2", "WP3", "WP4"},
+        PlayerID   = 2,       -- Partei des Hafen
+        Path       = "SH2WP", -- Pfad (auch als Table möglich)
+        Duration   = 4*60,     -- Ankerzeit in Sekunden
+        Interval   = 4,       -- Monate zwischen zwei Anfarten
+        OfferCount = 2,       -- Anzahl Angebote (1 bis 4)
         Offers = {
-            {
-                {"G_Gems", 5,},
-                {"G_Iron", 5,},
-                {"G_Beer", 2,},
-            },
-            {
-                {"G_Stone", 5,},
-                {"G_Sheep", 1,},
-                {"G_Cheese", 2,},
-                {"G_Milk", 5,},
-            },
-            {
-                {"G_Grain", 5,},
-                {"G_Broom", 2,},
-                {"G_Sheep", 1,},
-            },
-            {
-                {"U_CatapultCart", 1,},
-                {"U_MilitarySword", 3,},
-                {"U_MilitaryBow", 3,},
-            },
+            -- Angebot, Menge
+            {"G_Gems", 5},
+            {"G_Iron", 5},
+            {"G_Beer", 2},
+            {"G_Stone", 5},
+            {"G_Sheep", 1},
+            {"G_Cheese", 2},
+            {"G_Milk", 5},
+            {"G_Grain", 5},
+            {"G_Broom", 2},
+            {"U_CatapultCart", 1},
+            {"U_MilitarySword", 3},
+            {"U_MilitaryBow", 3}
         },
     };
     API.TravelingSalesmanCreate(TraderDescription);
-end
+end 
+
+function CreateHabor3()
+    local TraderDescription = {
+        PlayerID   = 3,       -- Partei des Hafen
+        Path       = "SH3WP", -- Pfad (auch als Table möglich)
+        Duration   = 3*60,     -- Ankerzeit in Sekunden
+        Interval   = 1,       -- Monate zwischen zwei Anfarten
+        OfferCount = 3,       -- Anzahl Angebote (1 bis 4)
+        Offers = {
+            -- Angebot, Menge
+            {"G_Salt", 5},
+            {"G_Stone", 5},
+            {"G_Beer", 2},
+            {"G_Stone", 5},
+            {"G_Sheep", 1},
+            {"G_Bread", 2},
+            {"G_Milk", 5},
+            {"G_Grain", 5},
+            {"G_Soap", 2},
+            {"U_SiegeTowerCart", 1},
+            {"U_MilitaryBandit_Melee_SE", 3},
+            {"U_MilitaryBandit_Melee_NE", 3}
+        },
+    };
+    API.TravelingSalesmanCreate(TraderDescription);
+end 
+
+function CreateHabor4()
+    local TraderDescription = {
+        PlayerID   = 4,       -- Partei des Hafen
+        Path       = "SH4WP", -- Pfad (auch als Table möglich)
+        Duration   = 5*60,     -- Ankerzeit in Sekunden
+        Interval   = 2,       -- Monate zwischen zwei Anfarten
+        OfferCount = 4,       -- Anzahl Angebote (1 bis 4)
+        Offers = {
+            -- Angebot, Menge
+            {"G_Dye", 5},
+            {"G_Herb", 5},
+            {"G_Beer", 2},
+            {"G_Stone", 5},
+            {"G_Cow", 1},
+            {"G_Bread", 2},
+            {"G_Milk", 5},
+            {"G_RawFish", 5},
+            {"G_Broom", 2},
+            {"U_SiegeTowerCart", 1},
+            {"U_MilitaryBandit_Melee_ME", 3},
+            {"U_MilitaryBandit_Melee_NA", 3}
+        },
+    };
+    API.TravelingSalesmanCreate(TraderDescription);
+end 
+
+function CreateHabor5()
+    local TraderDescription = {
+        PlayerID   = 5,       -- Partei des Hafen
+        Path       = "SH5WP", -- Pfad (auch als Table möglich)
+        Duration   = 7*60,     -- Ankerzeit in Sekunden
+        Interval   = 3,       -- Monate zwischen zwei Anfarten
+        OfferCount = 1,       -- Anzahl Angebote (1 bis 4)
+        Offers = {
+            -- Angebot, Menge
+            {"G_Salt", 5},
+            {"G_Herb", 5},
+            {"G_Beer", 2},
+            {"G_Gems", 5},
+            {"G_Cow", 1},
+            {"G_Bread", 2},
+            {"G_Milk", 5},
+            {"G_RawFish", 5},
+            {"G_Broom", 2},
+            {"U_CatapultCart", 1},
+            {"U_MilitaryBandit_Ranged_SE", 3},
+            {"U_MilitaryBandit_Ranged_NE", 3}
+        },
+    };
+    API.TravelingSalesmanCreate(TraderDescription);
+end 
