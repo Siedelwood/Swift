1a153b4f0737ca1179c0c6063a887c84736982a5
Martin Heller <n3crosh8d3@gmail.com>
2021-04-04 20:01:18 +0200

    adds more relations to diplomacy goal

--- qsb/core/selfload.lua
+++ qsb/core/selfload.lua
@@ -33,6 +33,8 @@ if not MapEditor and not GUI then
         gvMission = gvMission or {};
         gvMission.GlobalVariables = Logic.CreateReferenceToTableInGlobaLuaState("gvMission");
         gvMission.ContentPath = "maps/]] ..MapTypeFolder.. [[/" ..Framework.GetCurrentMapName() .. "/";
+        gvMission.MusicRootPath = "music/";
+        gvMission.PlaylistRootPath = "config/sound/";
 
         Script.Load(gvMission.ContentPath.. "questsystembehavior.lua");
         if Mission_LoadFiles then
