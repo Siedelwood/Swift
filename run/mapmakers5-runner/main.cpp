52e400ef32ef17b8b217d68b31e6ad55378118bc
Martin Heller <n3crosh8d3@gmail.com>
2021-10-02 18:22:06 +0200

    Add QSB S5 installer

--- run/mapmakers5-runner/main.cpp
+++ run/mapmakers5-runner/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 int main(int argc, char** argv) {
-	system("start /b javaw -Dfile.encoding=UTF8 -jar \"%cd%/QuestSystemBehavior.jar\"");
+	// system("start /b javaw -Dfile.encoding=UTF8 -jar \"%cd%/QuestSystemBehavior.jar\"");
+	system("start /b jre/bin/javaw -Dspring.profiles.active=s5mapmaker -Dfile.encoding=UTF8 -jar \"%cd%/updater.jar\"");
 	return 0;
 }
