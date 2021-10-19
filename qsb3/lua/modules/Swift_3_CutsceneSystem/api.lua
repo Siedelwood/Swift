eb0be4c035ac3de752045ad52959ff2d8145f2e3
Martin Heller <martin.heller@intecsoft.de>
2021-10-19 10:21:44 +0200

    Migrate cutscene system

---
+++ qsb3/lua/modules/Swift_3_CutsceneSystem/api.lua
@@ -0,0 +1,108 @@
+--[[
+Swift_3_CutsceneSystem/API
+
+Copyright (C) 2021 totalwarANGEL - All Rights Reserved.
+
+This file is part of Swift. Swift is created by totalwarANGEL.
+You may use and modify this file unter the terms of the MIT licence.
+(See https://en.wikipedia.org/wiki/MIT_License)
+]]
+
+---
+-- Ermöglicht es Cutscene zu verwenden.
+--
+-- <b>Vorausgesetzte Module:</b>
+-- <ul>
+-- <li><a href="Swift_1_DisplayCore.api.html">(1) Display Core</a></li>
+-- <li><a href="Swift_1_JobsCore.api.html">(1) Jobs Core</a></li>
+-- </ul>
+--
+-- @within Beschreibung
+-- @set sort=true
+--
+
+---
+--
+--
+function API.StartCutscene(_Cutscene, _Name, _PlayerID)
+    if GUI then
+        return;
+    end
+    local PlayerID = _PlayerID;
+    if not PlayerID and not Framework.IsNetworkGame() then
+        PlayerID = QSB.HumanPlayerID;
+    end
+    if type(_Cutscene) ~= "table" then
+        local Name = "Cutscene #" ..(ModuleCutsceneSystem.Global.CutsceneCounter +1);
+        error("API.StartCutscene (" ..Name.. "): _Cutscene must be a table!");
+        return;
+    end
+    if #_Cutscene == 0 then
+        local Name = "Cutscene #" ..(ModuleCutsceneSystem.Global.CutsceneCounter +1);
+        error("API.StartCutscene (" ..Name.. "): _Cutscene does not contain pages!");
+        return;
+    end
+    for i=1, #_Cutscene do
+        if not _Cutscene[i].__Legit then
+            local Name = "Cutscene #" ..(ModuleCutsceneSystem.Global.CutsceneCounter +1);
+            error("API.StartCutscene (" ..Name.. ", Page #" ..i.. "): Page is not initialized!");
+            return;
+        end
+    end
+    ModuleCutsceneSystem.Global:StartCutscene(_Name, PlayerID, _Cutscene);
+end
+
+---
+-- Erzeugt die Funktion zur Erstellung von Flights in einer Cutscene. Diese
+-- Funktion muss vor dem Start einer Cutscene aufgerufen werden, damit Seiten
+-- gebunden werden können.
+--
+-- @param[type=table] _Briefing Briefing Definition
+-- @return[type=function] <a href="#AF">AF</a>
+-- @within Anwenderfunktionen
+--
+-- @usage
+-- local AF = API.AddCutscenePages(Briefing);
+--
+function API.AddCutscenePages(_Cutscene)
+    _Cutscene.GetPage = function(self, _PlayerID, _NameOrID)
+        local ID = ModuleCutsceneSystem.Global:GetPageIDByName(_PlayerID, _NameOrID);
+        return ModuleCutsceneSystem.Global.Cutscene[_PlayerID][ID];
+    end
+
+    local AF = function(_Page)
+        if type(_Page) == "table" then
+            _Page.__Legit = true;
+            
+            -- Language
+            _Page.Title = API.ConvertPlaceholders(API.Localize(_Page.Title));
+            if _Page.Text then
+                _Page.Text = API.ConvertPlaceholders(API.Localize(_Page.Text));
+            end
+            if _Page.Lines then
+                _Page.Lines = API.ConvertPlaceholders(API.Localize(_Page.Lines));
+            end
+            if not _Page.Lines and not _Page.Text then
+                local Name = "Cutscene #" ..(ModuleCutsceneSystem.Global.CutsceneCounter +1);
+                error("AF (" ..Name.. ", Page #" ..(#_Cutscene+1).. "): Missing Lines or Text attribute!");
+                return;
+            end
+
+            -- Bars
+            if _Page.BigBars == nil then
+                _Page.BigBars = false;
+            end
+        end
+        table.insert(_Cutscene, _Page);
+        return _Page;
+    end
+    return AF;
+end
+
+---
+--
+--BriefingBriefing
+function AF(_Data)
+    error("AF (Cutscene System): not bound to a cutscene!");
+end
+
---
+++ qsb3/lua/modules/Swift_3_CutsceneSystem/behavior.lua
@@ -0,0 +1,181 @@
+--[[
+Swift_3_CutsceneSystem/Behavior
+
+Copyright (C) 2021 totalwarANGEL - All Rights Reserved.
+
+This file is part of Swift. Swift is created by totalwarANGEL.
+You may use and modify this file unter the terms of the MIT licence.
+(See https://en.wikipedia.org/wiki/MIT_License)
+]]
+
+---
+-- Fügt Behavior zur Steuerung von Cutscenes hinzu.
+--
+-- @set sort=true
+--
+
+-- -------------------------------------------------------------------------- --
+
+---
+-- Ruft die Funktion auf und startet das enthaltene Cutscene.
+--
+-- Jede Cutscene braucht einen eindeutigen Namen!
+--
+-- @param[type=string] _Name   Bezeichner des Cutscene
+-- @param[type=string] _Cutscene Funktionsname als String
+-- @within Reprisal
+--
+function Reprisal_Cutscene(...)
+    return B_Reprisal_Cutscene:new(...);
+end
+
+B_Reprisal_Cutscene = {
+    Name = "Reprisal_Cutscene",
+    Description = {
+        en = "Reprisal: Calls a function to start an new Cutscene.",
+        de = "Lohn: Ruft die Funktion auf und startet die enthaltene Cutscene.",
+    },
+    Parameter = {
+        { ParameterType.Default, en = "Cutscene function", de = "Funktion mit Cutscene" },
+    },
+}
+
+function B_Reprisal_Cutscene:GetReprisalTable()
+    return { Reprisal.Custom,{self, self.CustomFunction} }
+end
+
+function B_Reprisal_Cutscene:AddParameter(_Index, _Parameter)
+    if (_Index == 0) then
+        self.CutsceneName = _Parameter;
+    elseif (_Index == 1) then
+        self.Function = _Parameter;
+    end
+end
+
+function B_Reprisal_Cutscene:CustomFunction(_Quest)
+    _G[self.Function](self, self.CutsceneName, _Quest.ReceivingPlayer);
+end
+
+function B_Reprisal_Cutscene:Debug(_Quest)
+    if self.CutsceneName == nil or self.CutsceneName == "" then
+        error(string.format("%s: %s: Dialog name is invalid!", _Quest.Identifier, self.Name));
+        return true;
+    end
+    if not type(_G[self.Function]) == "function" then
+        error(_Quest.Identifier..": "..self.Name..": '"..self.Function.."' was not found!");
+        return true;
+    end
+    return false;
+end
+
+Swift:RegisterBehavior(B_Reprisal_Cutscene);
+
+-- -------------------------------------------------------------------------- --
+
+---
+-- Ruft die Funktion auf und startet das enthaltene Cutscene.
+--
+-- Jede Cutscene braucht einen eindeutigen Namen!
+--
+-- @param[type=string] _Name   Bezeichner des Cutscene
+-- @param[type=string] _Cutscene Funktionsname als String
+-- @within Reward
+--
+function Reward_Cutscene(...)
+    return B_Reward_Cutscene:new(...);
+end
+
+B_Reward_Cutscene = Swift:CopyTable(B_Reprisal_Cutscene);
+B_Reward_Cutscene.Name = "Reward_Cutscene";
+B_Reward_Cutscene.Description.en = "Reward: Calls a function to start an new Cutscene.";
+B_Reward_Cutscene.Description.de = "Lohn: Ruft die Funktion auf und startet die enthaltene Cutscene.";
+B_Reward_Cutscene.GetReprisalTable = nil;
+
+B_Reward_Cutscene.GetRewardTable = function(self, _Quest)
+    return { Reward.Custom,{self, self.CustomFunction} }
+end
+
+B_Reward_Cutscene.CustomFunction = function(self, _Quest)
+    _G[self.Function](self, self.CutsceneName, _Quest.ReceivingPlayer);
+end
+
+Swift:RegisterBehavior(B_Reward_Cutscene);
+
+-- -------------------------------------------------------------------------- --
+
+---
+-- Prüft, ob ein Cutscene beendet ist und startet dann den Quest.
+--
+-- @param[type=string] _Name     Bezeichner des Cutscene
+-- @param[type=number] _Waittime (optional) Wartezeit in Sekunden
+-- @within Trigger
+--
+function Trigger_Cutscene(...)
+    return B_Trigger_Cutscene:new(...);
+end
+
+B_Trigger_Cutscene = {
+    Name = "Trigger_Cutscene",
+    Description = {
+        en = "Trigger: Checks if an Cutscene has concluded and starts the quest if so.",
+        de = "Ausloeser: Prüft, ob eine Cutscene beendet ist und startet dann den Quest.",
+    },
+    Parameter = {
+        { ParameterType.Default, en = "Cutscene name", de = "Name des Cutscene" },
+        { ParameterType.Number,  en = "Wait time",     de = "Wartezeit" },
+    },
+}
+
+function B_Trigger_Cutscene:GetTriggerTable()
+    return { Triggers.Custom2,{self, self.CustomFunction} }
+end
+
+function B_Trigger_Cutscene:AddParameter(_Index, _Parameter)
+    if (_Index == 0) then
+        self.CutsceneName = _Parameter;
+    elseif (_Index == 2) then
+        _Parameter = _Parameter or 0;
+        self.WaitTime = _Parameter * 1;
+    end
+end
+
+function B_Trigger_Cutscene:CustomFunction(_Quest)
+    if API.GetCinematicEventStatus(self.CutsceneName) == CinematicEventStatus.Concluded then
+        if self.WaitTime and self.WaitTime > 0 then
+            self.WaitTimeTimer = self.WaitTimeTimer or Logic.GetTime();
+            if Logic.GetTime() >= self.WaitTimeTimer + self.WaitTime then
+                return true;
+            end
+        else
+            return true;
+        end
+    end
+    return false;
+end
+
+function B_Trigger_Cutscene:Debug(_Quest)
+    if self.WaitTime < 0 then
+        error(string.format("%s: %s: Wait time must be 0 or greater!", _Quest.Identifier, self.Name));
+        return true;
+    end
+    if self.CutsceneName == nil or self.CutsceneName == "" then
+        error(string.format("%s: %s: Dialog name is invalid!", _Quest.Identifier, self.Name));
+        return true;
+    end
+    local EventPlayerID = API.GetCinematicEventPlayerID(self.CutsceneName);
+    if EventPlayerID ~= _Quest.ReceivingPlayer then
+        error(string.format(
+            "%s: %s: Dialog '%s' is for player %d but quest is for player %d!",
+            _Quest.Identifier,
+            self.Name,
+            self.CutsceneName,
+            EventPlayerID,
+            _Quest.ReceivingPlayer
+        ));
+        return true;
+    end
+    return false;
+end
+
+Swift:RegisterBehavior(B_Trigger_Cutscene);
+
