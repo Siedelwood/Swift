8cc5d8d87e24de2b6dcebf54b1c39684cb924ea0
Martin Heller <n3crosh8d3@gmail.com>
2022-01-16 12:08:31 +0100

    Improve entity movement

--- qsb3/lua/modules/Swift_2_EntityMovement/api.lua
+++ qsb3/lua/modules/Swift_2_EntityMovement/api.lua
@@ -9,7 +9,10 @@ You may use and modify this file unter the terms of the MIT licence.
 ]]
 
 ---
--- 
+-- Ein Modul für die Bewegung von Entities.
+--
+-- Es werden Funktionen für den Endanwender sowie eine Wegfindung für andere
+-- Module bereitgestellt.
 --
 -- <b>Vorausgesetzte Module:</b>
 -- <ul>
@@ -24,8 +27,9 @@ You may use and modify this file unter the terms of the MIT licence.
 ---
 -- Events, auf die reagiert werden kann.
 --
--- @field EntityArrived      Ein Entity hat das Ziel erreicht. (Parameter: EntityID, TargetID, TargetX, TargetY)
--- @field EntityStuck        Ein Entity kann das Ziel nicht erreichen. (Parameter: EntityID, TargetID, TargetX, TargetY)
+-- @field EntityArrived       Ein Entity hat das Ziel erreicht. (Parameter: EntityID, Position, DataIndex)
+-- @field EntityStuck         Ein Entity kann das Ziel nicht erreichen. (Parameter: EntityID, Position, DataIndex)
+-- @field EntityAtCheckpoint  Ein Entity kann das Ziel nicht erreichen. (Parameter: EntityID, Position, DataIndex)
 -- @field PathFindingFinished Ein Pfad wurde erfolgreich gefunden (Parameter: PathIndex)
 -- @field PathFindingFailed   Ein Pfad konnte nicht ermittelt werden (Parameter: PathIndex)
 --
@@ -43,7 +47,7 @@ QSB.ScriptEvents = QSB.ScriptEvents or {};
 -- wird das Event QSB.ScriptEvents.EntityArrived geworfen.
 --
 -- @param               _Entity         Bewegtes Entity (Skriptname oder ID)
--- @param               _Target         Ziel (Skriptname, ID oder Position)
+-- @param               _Target         Ziel (Skriptname oder ID)
 -- @param[type=boolean] _IgnoreBlocking Direkten Weg benutzen
 -- @within Position
 --
@@ -53,47 +57,18 @@ function API.MoveEntity(_Entity, _Target, _IgnoreBlocking)
         error("API.MoveEntity: entity '" ..tostring(_Entity).. "' does not exist!");
         return;
     end
-
-    local TargetID = 0;
-    local Target;
-    if type(_Target) ~= "table" then
-        local ID2 = GetID(_Target);
-        if not IsExisting(ID2) then
-            error("API.MoveEntity: target '" ..tostring(_Target).. "' does not exist!");
-            return;
-        end
-        TargetID = ID2;
-        local x,y,z = Logic.EntityGetPos(ID2);
-        if Logic.IsBuilding(ID2) == 1 then
-            x,y = Logic.GetBuildingApproachPosition(ID2);
-        end
-        Target = {X= x, Y= y};
-    else
-        if not IsValidPosition(_Target) then
-            error("API.MoveEntity: target position is invalid!");
-            return;
-        end
-        Target = _Target;
+    local ID2 = GetID(_Target);
+    if not IsExisting(ID2) then
+        error("API.MoveEntity: entity '" ..tostring(_Target).. "' does not exist!");
+        return;
     end
-
-    if _IgnoreBlocking then
-        if Logic.IsSettler(ID1) == 1 then
-            Logic.SetTaskList(ID1, TaskLists.TL_NPC_WALK);
-        end
-        Logic.MoveEntity(ID1, Target.X, Target.Y);
-    else
-        Logic.MoveSettler(ID1, Target.X, Target.Y);
-    end
-
-    return API.StartHiResJob(
-        function(_ID1, _ID2, _X, _Y)
-            ModulePathing.Global:MoveEntityController(_ID1, _ID2, _X, _Y)
-        end,
-        ID1,
-        TargetID,
-        Target.X,
-        Target.Y
+    local Index = ModulePathing.Global:FillMovingEntityDataForController(
+        _Entity, {_Target}, nil, nil, _IgnoreBlocking
     );
+    API.StartHiResJob(function(_Index)
+        return ModulePathing.Global:MoveEntityPathController(_Index);
+    end, Index);
+    return Index;
 end
 
 ---
@@ -106,7 +81,7 @@ end
 -- wird das Event QSB.ScriptEvents.EntityArrived geworfen.
 --
 -- @param               _Entity         Bewegtes Entity (Skriptname oder ID)
--- @param               _Target         Ziel (Skriptname, ID oder Position)
+-- @param               _Target         Ziel (Skriptname oder ID)
 -- @param               _LookAt         Angeschaute Position (Skriptname, ID oder Position)
 -- @param[type=boolean] _IgnoreBlocking Direkten Weg benutzen
 -- @within Anwenderfunktionen
@@ -117,18 +92,18 @@ function API.MoveEntityAndLookAt(_Entity, _Target, _LookAt, _IgnoreBlocking)
         error("API.MoveEntityAndLookAt: entity '" ..tostring(_Entity).. "' does not exist!");
         return;
     end
-    local ID2 = GetID(_LookAt);
+    local ID2 = GetID(_Target);
     if not IsExisting(ID2) then
-        error("API.MoveEntityAndLookAt: entity '" ..tostring(_LookAt).. "' does not exist!");
-        return;
-    end
-    local ID3 = GetID(_Target);
-    if not IsExisting(ID3) then
         error("API.MoveEntityAndLookAt: entity '" ..tostring(_Target).. "' does not exist!");
         return;
     end
-    ModulePathing.Global.MovingEntities[ID1] = ID2;
-    API.MoveEntity(_Entity, _Target, _IgnoreBlocking);
+    local Index = ModulePathing.Global:FillMovingEntityDataForController(
+        _Entity, {_Target}, _LookAt, nil, _IgnoreBlocking
+    );
+    API.StartHiResJob(function(_Index)
+        return ModulePathing.Global:MoveEntityPathController(_Index);
+    end, Index);
+    return Index;
 end
 
 ---
@@ -141,7 +116,7 @@ end
 -- wird das Event QSB.ScriptEvents.EntityArrived geworfen.
 --
 -- @param                _Entity         Bewegtes Entity (Skriptname oder ID)
--- @param                _Target         Ziel (Skriptname, ID oder Position)
+-- @param                _Target         Ziel (Skriptname oder ID)
 -- @param[type=function] _Action         Funktion wenn Entity ankommt
 -- @param[type=boolean]  _IgnoreBlocking Direkten Weg benutzen
 -- @within Anwenderfunktionen
@@ -157,7 +132,48 @@ function API.MoveEntityAndExecute(_Entity, _Target, _Action, _IgnoreBlocking)
         error("API.MoveEntityAndExecute: entity '" ..tostring(_Target).. "' does not exist!");
         return;
     end
-    ModulePathing.Global.MovingEntities[ID1] = _Action;
-    API.MoveEntity(_Entity, _Target, _IgnoreBlocking);
+    local Index = ModulePathing.Global:FillMovingEntityDataForController(
+        _Entity, {_Target}, nil, _Action, _IgnoreBlocking
+    );
+    API.StartHiResJob(function(_Index)
+        return ModulePathing.Global:MoveEntityPathController(_Index);
+    end, Index);
+    return Index;
+end
+
+---
+-- Bewegt ein Entity über den angegebenen Pfad.
+--
+-- Wenn das Ziel zu irgend einem Zeitpunkt nicht erreicht werden kann, wird die
+-- Bewegung abgebrochen und das Event QSB.ScriptEvents.EntityStuck geworfen.
+--
+-- Jedes Mal wenn das Entity einen Wegpunkt erreicht hat, wird das Event
+-- QSB.ScriptEvents.EntityAtCheckpoint geworfen.
+--
+-- Das Ziel gilt als erreicht, sobald sich das Entity nicht mehr bewegt. Dann
+-- wird das Event QSB.ScriptEvents.EntityArrived geworfen.
+--
+-- @param                _Entity         Bewegtes Entity (Skriptname oder ID)
+-- @param                _Targets        Liste mit Wegpunkten
+-- @param[type=boolean]  _IgnoreBlocking Direkten Weg benutzen
+-- @within Anwenderfunktionen
+--
+function API.MoveEntityOnCheckpoints(_Entity, _Targets, _IgnoreBlocking)
+    local ID1 = GetID(_Entity);
+    if not IsExisting(ID1) then
+        error("API.MoveEntityOnCheckpoints: entity '" ..tostring(_Entity).. "' does not exist!");
+        return;
+    end
+    if type(_Targets) ~= "table" then
+        error("API.MoveEntityOnCheckpoints: target list must be a table!");
+        return;
+    end
+    local Index = ModulePathing.Global:FillMovingEntityDataForController(
+        _Entity, _Targets, nil, nil, _IgnoreBlocking
+    );
+    API.StartHiResJob(function(_Index)
+        return ModulePathing.Global:MoveEntityPathController(_Index);
+    end, Index);
+    return Index;
 end
 
