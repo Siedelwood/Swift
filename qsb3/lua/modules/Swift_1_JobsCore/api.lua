e5cc9efcdf52d0ccae65b9f94c1ccf4315c25ea9
Martin Heller <n3crosh8d3@gmail.com>
2022-02-06 12:40:26 +0100

    Update copyright

--- qsb3/lua/modules/Swift_1_JobsCore/api.lua
+++ qsb3/lua/modules/Swift_1_JobsCore/api.lua
@@ -1,4 +1,12 @@
--- Job API ------------------------------------------------------------------ --
+--[[
+Swift_1_JobsCore/API
+
+Copyright (C) 2021 - 2022 totalwarANGEL - All Rights Reserved.
+
+This file is part of Swift. Swift is created by totalwarANGEL.
+You may use and modify this file unter the terms of the MIT licence.
+(See https://en.wikipedia.org/wiki/MIT_License)
+]]
 
 ---
 -- Dieses Modul erweitert die standardmäßig vorhandenen Jobs. Du kannst für
--- qsb3/lua/modules/Swift_2_Quests/source.lua
+++ qsb3/lua/modules/Swift_2_Quests/source.lua
@@ -1,6 +1,12 @@
--- -------------------------------------------------------------------------- --
--- Module Quests                                                              --
--- -------------------------------------------------------------------------- --
+--[[
+Swift_2_Quests/Source
+
+Copyright (C) 2021 - 2022 totalwarANGEL - All Rights Reserved.
+
+This file is part of Swift. Swift is created by totalwarANGEL.
+You may use and modify this file unter the terms of the MIT licence.
+(See https://en.wikipedia.org/wiki/MIT_License)
+]]
 
 ModuleQuests = {
     Properties = {
