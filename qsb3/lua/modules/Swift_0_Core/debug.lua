1716ff9c57eebb33e9872bd6d8a00bc2cc9e7d43
totalwarANGEL <n3crosh8d3@gmail.com>
2022-07-20 17:30:44 +0200

    Add event explaination

--- qsb3/lua/modules/Swift_0_Core/debug.lua
+++ qsb3/lua/modules/Swift_0_Core/debug.lua
@@ -8,6 +8,319 @@ You may use and modify this file unter the terms of the MIT licence.
 (See https://en.wikipedia.org/wiki/MIT_License)
 ]]
 
+---
+-- Stellt Cheats und Befehle für einfacheres Testen bereit.
+--
+-- <b>Befehle:</b><br>
+-- <i>Diese Befehle können über die Konsole (SHIFT + ^) eingegeben werden, wenn
+-- der Debug Mode aktiviert ist.</i><br>
+-- <table border="1">
+-- <tr>
+-- <td><b>Befehl</b></td>
+-- <td><b>Beschreibung</b></td>
+-- </tr>
+-- <tr>
+-- <td>restartmap</td>
+-- <td>Map sofort neu starten</td>
+-- </tr>
+-- </table>
+--
+-- <b>Cheats:</b><br>
+-- <i>Bei aktivierten Debug Mode können diese Cheat Codes verwendet werden.</i><br>
+-- <table border="1">
+-- <tr>
+-- <td><b>Cheat</b></td>
+-- <td><b>Beschreibung</b></td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + ^</td>
+-- <td>Konsole öffnen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + ALT + R</td>
+-- <td>Map sofort neu starten.</td>
+-- </tr>
+-- <td>CTRL + C</td>
+-- <td>Zeitanzeige an/aus</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + A</td>
+-- <td>Clutter (Gräser) anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + C</td>
+-- <td>Grasobjekte anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + E</td>
+-- <td>Laubbäume anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + F</td>
+-- <td>FoW anzeigen (an/aus) <i>Gebiete werden dauerhaft erkundet!</i></td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + G</td>
+-- <td>GUI anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + H</td>
+-- <td>Steine und Tannen anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + R</td>
+-- <td>Straßen anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + S</td>
+-- <td>Schatten anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + T</td>
+-- <td>Boden anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + U</td>
+-- <td>FoW anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + W</td>
+-- <td>Wasser anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + X</td>
+-- <td>Render Mode des Wassers umschalten (Einfach und komplex)</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + Y</td>
+-- <td>Himmel anzeigen (an/aus)</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F10</td>
+-- <td>Selektiertes Gebäude anzünden</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F11</td>
+-- <td>Selektierte Einheit verwunden</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F12</td>
+-- <td>Alle Rechte freigeben / wieder sperren</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + 1</td>
+-- <td>FPS-Anzeige</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 4</td>
+-- <td>Bogenschützen unter der Maus spawnen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 5</td>
+-- <td>Schwertkämpfer unter der Maus spawnen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 6</td>
+-- <td>Katapultkarren unter der Maus spawnen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 7</td>
+-- <td>Ramme unter der Maus spawnen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 8</td>
+-- <td>Belagerungsturm unter der Maus spawnen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 9</td>
+-- <td>Katapult unter der Maus spawnen</td>
+-- </tr>
+-- <tr>
+-- <td>(Num) +</td>
+-- <td>Spiel beschleunigen</td>
+-- </tr>
+-- <tr>
+-- <td>(Num) -</td>
+-- <td>Spiel verlangsamen</td>
+-- </tr>
+-- <tr>
+-- <td>(Num) *</td>
+-- <td>Geschwindigkeit zurücksetzen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F1</td>
+-- <td>+ 50 Gold</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F2</td>
+-- <td>+ 10 Holz</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F3</td>
+-- <td>+ 10 Stein</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F4</td>
+-- <td>+ 10 Getreide</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F5</td>
+-- <td>+ 10 Milch</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F6</td>
+-- <td>+ 10 Kräuter</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F7</td>
+-- <td>+ 10 Wolle</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F8</td>
+-- <td>+ 10 auf alle Waren</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F1</td>
+-- <td>+ 10 Honig</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F2</td>
+-- <td>+ 10 Eisen</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F3</td>
+-- <td>+ 10 Fisch</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F4</td>
+-- <td>+ 10 Wild</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F5</td>
+-- <td>Bedürfnis nach Nahrung in Gebäude aktivieren</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F6</td>
+-- <td>Bedürfnis nach Kleidung in Gebäude aktivieren</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F7</td>
+-- <td>Bedürfnis nach Hygiene in Gebäude aktivieren</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F8</td>
+-- <td>Bedürfnis nach Unterhaltung in Gebäude aktivieren</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F9</td>
+-- <td>Nahrung für selektiertes Gebäude erhöhen</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F9</td>
+-- <td>Nahrung für selektiertes Gebäude verringern</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F10</td>
+-- <td>Kleidung für selektiertes Gebäude erhöhen</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F10</td>
+-- <td>Kleidung für selektiertes Gebäude verringern</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F11</td>
+-- <td>Hygiene für selektiertes Gebäude erhöhen</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F11</td>
+-- <td>Hygiene für selektiertes Gebäude verringern</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + F12</td>
+-- <td>Unterhaltung für selektiertes Gebäude erhöhen</td>
+-- </tr>
+-- <tr>
+-- <td>SHIFT + F12</td>
+-- <td>Unterhaltung für selektiertes Gebäude verringern</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + CTRL + F10</td>
+-- <td>Einnahmen des selektierten Gebäudes erhöhen</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + (Num) 1</td>
+-- <td>Burg selektiert → Gold verringern, Werkstatt selektiert → Ware verringern</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + (Num) 2</td>
+-- <td>Burg selektiert → Gold erhöhen, Werkstatt selektiert → Ware erhöhen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 1</td>
+-- <td>Kontrolle über Spieler 1</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 2</td>
+-- <td>Kontrolle über Spieler 2</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 3</td>
+-- <td>Kontrolle über Spieler 3</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 4</td>
+-- <td>Kontrolle über Spieler 4</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 5</td>
+-- <td>Kontrolle über Spieler 5</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 6</td>
+-- <td>Kontrolle über Spieler 6</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 7</td>
+-- <td>Kontrolle über Spieler 7</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + ALT + 8</td>
+-- <td>Kontrolle über Spieler 8</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 0</td>
+-- <td>Kamera durchschalten</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + (Num) 1</td>
+-- <td>Kamerasprünge im RTS-Mode</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + V</td>
+-- <td>Territorien anzeigen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + B</td>
+-- <td>Blocking anzeigen</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + N</td>
+-- <td>Gitter verstecken</td>
+-- </tr>
+-- <tr>
+-- <td>CTRL + SHIFT + F9</td>
+-- <td>DEBUG-Ausgabe einschalten</td>
+-- </tr>
+-- <tr>
+-- <td>ALT + F9</td>
+-- <td>Zufälligen Arbeiter verheiraten</td>
+-- </tr>
+-- </table>
+--
+-- @set sort=true
+-- @within Beschreibung
+--
+
 Swift.m_Benchmarks           = {};
 Swift.m_CheckAtRun           = false;
 Swift.m_TraceQuests          = false;
