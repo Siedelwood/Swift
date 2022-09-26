261c4fba33d40269b69a233b70e2459e6c70b8e3
totalwarANGEL <n3crosh8d3@gmail.com>
2022-09-26 17:31:11 +0200

    Raise version number

--- qsb3/lua/modules/Swift_0_Core/swift.lua
+++ qsb3/lua/modules/Swift_0_Core/swift.lua
@@ -14,7 +14,7 @@ SCP = SCP or {
     Core = {}
 };
 
-QSB.Version = "Version 3.0.0 BETA (1.1.17)";
+QSB.Version = "Version 3.0.0 BETA (1.1.18)";
 QSB.Language = "de";
 QSB.HumanPlayerID = 1;
 QSB.ScriptCommandSequence = 2;
