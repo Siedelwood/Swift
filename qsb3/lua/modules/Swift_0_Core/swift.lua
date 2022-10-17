b20e2d44b4fc5577d8650fd058e71da13c977e79
totalwarANGEL <n3crosh8d3@gmail.com>
2022-10-17 17:32:23 +0200

    Version 3.0.0 (Beta 1.1.18-9)

--- qsb3/lua/modules/Swift_0_Core/swift.lua
+++ qsb3/lua/modules/Swift_0_Core/swift.lua
@@ -14,7 +14,7 @@ SCP = SCP or {
     Core = {}
 };
 
-QSB.Version = "Version 3.0.0 BETA (1.1.18-3)";
+QSB.Version = "Version 3.0.0 BETA (1.1.18-9)";
 QSB.Language = "de";
 QSB.HumanPlayerID = 1;
 QSB.ScriptCommandSequence = 2;
